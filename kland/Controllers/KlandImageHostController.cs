8292278c39e425082dbfa4770917562793778a85
Carlos Sanchez <randomouscrap98@aol.com>
2021-11-15 13:04:12 -0500

    Added text endpoints

--- kland/Controllers/KlandImageHostController.cs
+++ kland/Controllers/KlandImageHostController.cs
@@ -9,6 +9,7 @@ public class KlandImageHostControllerConfig
 {
     public string? Bucket {get;set;}
     public string? IdRegex {get;set;}
+    public string? AIdRegex {get;set;}
     public string? ETagPrepend {get;set;}
 }
 
@@ -19,6 +20,7 @@ public class KlandImageHostController: KlandBase
     protected IAmazonS3 s3client;
     protected KlandImageHostControllerConfig config;
     protected Regex idRegex;
+    protected Regex aidRegex;
 
     public static readonly object ImageHashLock = new object();
     public static readonly object ThreadHashLock = new object();
@@ -36,28 +38,49 @@ public class KlandImageHostController: KlandBase
 
         idRegex = new Regex(config.IdRegex ?? throw new InvalidOperationException("No image ID regex!"), 
             RegexOptions.Compiled | RegexOptions.IgnoreCase);
+        aidRegex = new Regex(config.AIdRegex ?? throw new InvalidOperationException("No alt ID regex!"), 
+            RegexOptions.Compiled | RegexOptions.IgnoreCase);
     }
 
-    [HttpGet("i/{id}")]
-    [ResponseCache(Duration = 13824000)] //six months
-    public async Task<IActionResult> GetImageAsync([FromRoute] string id) //, [FromQuery] GetFileModify modify)
+    protected async Task<IActionResult> GetS3(string id, Regex regex, Func<Match, string> mimeGenerator)
     {
-        var match = idRegex.Match(id);
+        var match = regex.Match(id);
 
         if(!match.Success)
-            return BadRequest($"Image ID not in specified format: {config.IdRegex}");
+            return BadRequest($"S3 ID not in specified format: {regex.ToString()}");
 
         //Go get it from s3
         try
         {
             var obj = await s3client.GetObjectAsync(config.Bucket, id);
             Response.Headers.Add("ETag", config.ETagPrepend + id);
-            return File(obj.ResponseStream, "image/" + match.Groups[1].Value?.TrimStart('.')); 
+            return File(obj.ResponseStream, mimeGenerator(match));
+            //"image/" + match.Groups[1].Value?.TrimStart('.')); 
         }
         catch(Exception ex)
         {
-            logger.LogError($"Exception during image request: {ex}");
+            logger.LogError($"Exception during S3 request: {ex}");
             return NotFound();
         }
+    }
+
+    [HttpGet("i/{id}")]
+    [ResponseCache(Duration = 13824000)] //six months
+    public Task<IActionResult> GetImageAsync([FromRoute] string id) //, [FromQuery] GetFileModify modify)
+    {
+        return GetS3(id, idRegex, m => "image/" + m.Groups[1].Value?.TrimStart('.')); 
+    }
+
+    [HttpGet("a/{id}")]
+    [ResponseCache(Duration = 13824000)] //six months
+    public Task<IActionResult> GetTextAsync([FromRoute] string id)
+    {
+        return GetS3(id, aidRegex, m => "text/plain");
+    }
+
+    [HttpPost("uploadtext")]
+    public IActionResult PostText()
+    {
+        return BadRequest("Sorry, the animation uploader is not implemented anymore! If it's needed, it can be added again!");
     }
 }
